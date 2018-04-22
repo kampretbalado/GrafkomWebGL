c0f920cd369919cf838e4a564517e056266b8e99
Malik Ariefa <malik.ariefa@gmail.com>
2018-04-22 18:28:58 +0700

    rapih2 folder

--- js/Robot.js
+++ js/Robot.js
@@ -1,11 +1,35 @@
+/**
+    Program backend dari Robot.html
+    Program ini dibuat untuk memenuhi tugas Worksheet 2 mata kuliah grafika komputer
+
+    Dibuat oleh:
+    Muhammad Azmi Malik Ariefa - 1406623184 - malik.ariefa@gmail.com
+    Firas Atha Muhtadi - 1406579246 - vrasath@gmail.com
+ */
+
 "use strict";
 
 var canvas, gl, program;
 
-var NumVertices = 36; //(6 faces)(2 triangles/face)(3 vertices/triangle)
+var pointsArray = [];
+var colorsArray = [];
+var normalsArray = [];
 
-var points = [];
-var colors = [];
+var lightPosition = vec4(5.0, 5.0, 5.0, 0.0 );
+var lightAmbient = vec4(0.5, 0.5, 0.5, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0);
+var materialSpecular = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialShininess = 100.0;
+
+var ctm;
+var ambientColor, diffuseColor, specularColor;
+var viewerPos;
+
+var NumVertices = 36; //(6 faces)(2 triangles/face)(3 vertices/triangle)
 
 var vertices = [
     vec4( -0.5, -0.5,  0.5, 1.0 ),
@@ -90,18 +114,29 @@ var vBuffer, cBuffer;
 //----------------------------------------------------------------------------
 
 function quad(  a,  b,  c,  d ) {
-    colors.push(vertexColors[a]);
-    points.push(vertices[a]);
-    colors.push(vertexColors[a]);
-    points.push(vertices[b]);
-    colors.push(vertexColors[a]);
-    points.push(vertices[c]);
-    colors.push(vertexColors[a]);
-    points.push(vertices[a]);
-    colors.push(vertexColors[a]);
-    points.push(vertices[c]);
-    colors.push(vertexColors[a]);
-    points.push(vertices[d]);
+    var t1 = subtract(vertices[b], vertices[a]);
+    var t2 = subtract(vertices[c], vertices[b]);
+    var normal = cross(t1, t2);
+    var normal = vec3(normal);
+
+    colorsArray.push(vertexColors[a]);
+    pointsArray.push(vertices[a]);
+    normalsArray.push(normal);
+    colorsArray.push(vertexColors[a]);
+    pointsArray.push(vertices[b]);
+    normalsArray.push(normal);
+    colorsArray.push(vertexColors[a]);
+    pointsArray.push(vertices[c]);
+    normalsArray.push(normal);
+    colorsArray.push(vertexColors[a]);
+    pointsArray.push(vertices[a]);
+    normalsArray.push(normal);
+    colorsArray.push(vertexColors[a]);
+    pointsArray.push(vertices[c]);
+    normalsArray.push(normal);
+    colorsArray.push(vertexColors[a]);
+    pointsArray.push(vertices[d]);
+    normalsArray.push(normal);
 }
 
 
@@ -157,22 +192,53 @@ window.onload = function init() {
     gl.useProgram( program );
 
     // Create and initialize  buffer objects
+    
+    // normals
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
 
+    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    gl.vertexAttribPointer( vNormal, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vNormal );
+
+    // positions
     vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
 
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
+    //colors
     cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
 
     var vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vColor );
+
+    // shades
+    viewerPos = vec3(0.0, 0.0, -20.0 );
+
+    var ambientProduct = mult(lightAmbient, materialAmbient);
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
+
+    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"),
+       flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"),
+       flatten(diffuseProduct) );
+    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"),
+       flatten(specularProduct) );
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"),
+       flatten(lightPosition) );
+
+    gl.uniform1f(gl.getUniformLocation(program,
+       "shininess"),materialShininess);
+
 
     document.getElementById("snake1").onchange = function(event) {
         theta[0] = event.target.value;
@@ -223,7 +289,6 @@ var display = function () {
 
 //ROBOT FUNCTIONS--------------------------------------------------------------
 
-
 function base() {
     var s = scale4(robot.base.width, robot.base.height, robot.base.width);
     var instanceMatrix = mult( translate( 0.0, 0.5 * robot.base.height, 0.0 ), s);
@@ -250,11 +315,24 @@ function snakeBody() {
     gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
 }
 
-//---------------------------------------------------------------------------------
+//LIGHTBOX--------------------------------------------------------------------------
+
+function lightBox() {
+    var s = scale4(1.0, 1.0, 1.0);
+    var instanceMatrix = mult( translate( 1.0, 1.0, 1.0 ), s);
+    var t = mult(modelViewMatrix, instanceMatrix);
+    gl.uniformMatrix4fv(modelViewMatrixLoc,  false, flatten(t) );
+    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+}
+
+//----------------------------------------------------------------------------------
 
 var renderRobot = function() {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
         
+    modelViewMatrix = translate(lightPosition[0], lightPosition[1], lightPosition[2]);
+    lightBox();
+
     var threshold = 45;
 
     anim=anim+animSign;
@@ -310,6 +388,9 @@ var turnflag = false;
 var renderSnake = function() {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
     
+    modelViewMatrix = translate(lightPosition[0], lightPosition[1], lightPosition[2]);
+    lightBox();
+
     var threshold = 45;
 
     anim=anim+animSign;
@@ -322,7 +403,7 @@ var renderSnake = function() {
     //var wiggle =(anim % threshold*2) * Math.pow(-1,Math.floor(anim/threshold*2)) + threshold*2 * (Math.floor(anim/threshold*2)%2);
     //console.log(Math.pow(-1,Math.floor(anim/threshold*2)));
     var x=Number(theta[1]) + wiggle;
-    console.log(x);
+    //console.log(x);
     modelViewMatrix = rotate(theta[0], 0, 1, 0 );
     modelViewMatrix  = mult(modelViewMatrix, rotate(theta[2], 1, 0, 0) );
     modelViewMatrix  = mult(modelViewMatrix, rotate(x, 0, 0, 1) );
